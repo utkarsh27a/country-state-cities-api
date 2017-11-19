0baf4c83b04c0dc6d7cf903ff1294aeeae027c46
Utkarsh Pandey <utkarsh27a@gmail.com>
2017-11-19 21:28:43 +0530

    creating a index page to use api

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -1,15 +1,31 @@
+var baseUrl = "/api";
+
 var Location = {
   getCountries: function() {
-
+    $.get(baseUrl+"/countries").done(function(response) {
+      if (response.status == "success") {
+        Location.fill(response.data, $("#country"));
+      }
+      else {
+        alert(response.message);
+      }
+    })
   },
   getStates: function() {
-
+    $.get(baseUrl+"/states").done(function(response) {
+      if (response.status == "success") {
+        Location.fill(response.data, $("#state"));
+      }
+      else {
+        alert(response.message);
+      }
+    })
   },
   getCities: function() {
-
+    $.get(baseUrl+"/cities")
   },
   fill: function(data, target) {
-    var html = [];
+    var html = ['<option>Select</option>'];
     $(target).select2("destroy");
     $.each(data, function(i, element) {
         html.push('<option>'+element+'</option>');
@@ -19,5 +35,6 @@ var Location = {
   },
   init: function() {
      $('.select2').select2();
+     Location.getCountries();
   }
 }
