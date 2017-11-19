c72ddc83d53c68a649c5068adbf84c56e27dc4b4
Utkarsh Pandey <utkarsh27a@gmail.com>
2017-11-19 16:09:01 +0530

    states api

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var router = express.Router();
+countries = require("../db/countries");
+states = require("../db/states");
+cities = require("../db/cities");
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.json({
     version: 0.1
   });
+});
+
+router.get('/countries', function(req, res, next) {
+  res.json(countries);
+});
+
+router.get('/states', function(req, res, next) {
+  if (req.query.country == undefined) {
+    return res.json({
+      status: "fail",
+      message: "Invalid country",
+    });
+  }
+  var country = req.query.country.split(' ').join('_').toLocaleLowerCase();
+  console.log(country)
+  res.json(states[country]);
+});
+
+router.get('/cities', function(req, res, next) {
+  res.json(cities);
 });
 
 
